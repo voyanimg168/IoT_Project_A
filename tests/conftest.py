362bcdffaf4ee21fab749ae51be165c58949d7d9
voyanimg168 <minrugan@gmail.com>
2026-05-06 20:42:55 -0600

    Edit test.yml and Uncommented conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-# # tests/conftest.py
-# import threading
-# import pytest
-# from src.api_server import app  # adjust import to match your app
+# tests/conftest.py
+import threading
+import pytest
+from src.api_server import app  # adjust import to match your app
 
-# @pytest.fixture(scope="session", autouse=True)
-# def start_server():
-#     t = threading.Thread(target=lambda: app.run(port=5000))
-#     t.daemon = True
-#     t.start()
+@pytest.fixture(scope="session", autouse=True)
+def start_server():
+    t = threading.Thread(target=lambda: app.run(port=5000))
+    t.daemon = True
+    t.start()
